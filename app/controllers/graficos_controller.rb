c4409aa03c04bfe3a0c0522d288dd8aeae40b15b
Robin Weidlich <robinweidlich7@gmail.com>
2020-03-31 18:11:18 -0300

    nix erreicht

--- app/controllers/graficos_controller.rb
+++ app/controllers/graficos_controller.rb
@@ -1,4 +1,10 @@
 class GraficosController < ApplicationController
   def home
   end
+
+  def home2
+  end
+
+  def home3
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'graficos/home'
+  get 'graficos/home2'
+  get 'graficos/home3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
