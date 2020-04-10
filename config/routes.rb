97ac9ed6f0d2c8097aa6eeb945e22afdba6fa277
Robin Weidlich <robinweidlich7@gmail.com>
2020-04-10 17:39:44 -0300

    fix label radius problems and add label line break

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'graficos/home3'
   get 'graficos/home4'
   get 'graficos/home5'
+  get 'graficos/home6'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
