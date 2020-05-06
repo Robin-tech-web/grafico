f5157a6ef4674d3c5666454dace83bb7f18057d1
Robin Weidlich <robinweidlich7@gmail.com>
2020-05-06 17:00:17 -0300

    Det Teil funktionieeeeert

--- app/controllers/graficos_controller.rb
+++ app/controllers/graficos_controller.rb
@@ -21,7 +21,14 @@ class GraficosController < ApplicationController
   private
 
   def grafico_params
-    params.require(:grafico).permit(:sustainable, :self_organization, :technical_excellence)
+    params.require(:grafico).permit(:sustainable, :self_organization, :technical_excellence, :planning_and_estimating,
+      :effective_meetings, :size_and_skills, :allocation_and_stability, :workspace, :vision_and_purpose, :goals_and_outcomes,
+      :quarterly_roadmap, :monthly_release, :weekly_iteration, :roles_and_expectations, :generalizing_specialities,
+      :product_owner, :team, :stakeholder, :predictable_velocity, :time_to_market, :value_delivered,
+      :quality, :response_to_change, :effective_facilitation, :leadership_1, :impediment_management,
+      :leadership_2, :technical_expertise, :engagement, :backlog_management, :leadership_3,
+      :leadership_4, :develop_people, :process_imp, :happiness, :collaboration, :trust_and_respect,
+      :creativity_and_innovation, :accountability)
   end
 
 end
